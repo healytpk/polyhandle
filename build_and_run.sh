10b83dd01eeb41d628da245e1b73d74d2c042136
healytpk <thomaspkhealy@yahoo.com>
2025-06-06 00:35:19 +0100

    turn vtable discrimination back on

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -DNDEBUG -frtti
+clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -DNDEBUG -O3 -frtti \
+        -fptrauth-vtable-pointer-address-discrimination \
+        -fptrauth-vtable-pointer-type-discrimination
         #-fptrauth-calls \
-        #-fptrauth-vtable-pointer-address-discrimination \
-        #-fptrauth-vtable-pointer-type-discrimination \
         #-arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -fPIC
         #-fno-exceptions
 
 objdump -d research_machine_code.o | tee output_research_machine_code.txt
 
-clang++ -o prog research_machine_code.cpp -std=c++20 -DNDEBUG -frtti
+clang++ -o prog research_machine_code.cpp -std=c++20 -DNDEBUG -O3 -frtti \
+        -fptrauth-vtable-pointer-address-discrimination \
+        -fptrauth-vtable-pointer-type-discrimination
         #-fptrauth-calls \
-        #-fptrauth-vtable-pointer-address-discrimination \
-        #-fptrauth-vtable-pointer-type-discrimination \
         #-arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -fPIC
         #-fno-exceptions
 
