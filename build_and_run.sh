e06486baad90d1c43ae104757d60a31388447bef
healytpk <thomaspkhealy@yahoo.com>
2025-06-06 00:30:42 +0100

    remove optimisation flag O3

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -O3 -DNDEBUG \
+clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -DNDEBUG \
         -fptrauth-calls \
         -fptrauth-vtable-pointer-address-discrimination \
         -fptrauth-vtable-pointer-type-discrimination \
@@ -9,7 +9,7 @@ clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -O3 -
 
 objdump -d research_machine_code.o | tee output_research_machine_code.txt
 
-clang++ -o prog research_machine_code.cpp -std=c++20 -O3 -DNDEBUG \
+clang++ -o prog research_machine_code.cpp -std=c++20 -DNDEBUG \
         -fptrauth-calls \
         -fptrauth-vtable-pointer-address-discrimination \
         -fptrauth-vtable-pointer-type-discrimination \
