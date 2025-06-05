c16837a10bdee962ffddffe305b87e36a00c8118
healytpk <thomaspkhealy@yahoo.com>
2025-06-06 00:20:46 +0100

    Trying nopie instead of fPIC

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,8 +2,8 @@
 clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -O3 -DNDEBUG \
         -fptrauth-calls \
         -fptrauth-vtable-pointer-address-discrimination \
-        -fptrauth-vtable-pointer-address-discrimination -fptrauth-vtable-pointer-type-discrimination \
-        -frtti
+        -fptrauth-vtable-pointer-type-discrimination \
+        -frtti -nopie
         #-arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -fPIC
         #-fno-exceptions
 
@@ -12,8 +12,8 @@ objdump -d research_machine_code.o | tee output_research_machine_code.txt
 clang++ -o prog research_machine_code.cpp -std=c++20 -O3 -DNDEBUG \
         -fptrauth-calls \
         -fptrauth-vtable-pointer-address-discrimination \
-        -fptrauth-vtable-pointer-address-discrimination -fptrauth-vtable-pointer-type-discrimination \
-        -frtti
+        -fptrauth-vtable-pointer-type-discrimination \
+        -frtti -nopie
         #-arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -fPIC
         #-fno-exceptions
 
