0aec2ba9b30a83c4b31792f10b983a5018ddc311
healytpk <thomaspkhealy@yahoo.com>
2025-06-06 00:11:03 +0100

    remove stdlibc++ switch from compiler

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,7 +4,7 @@ clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -O3 -
         -fptrauth-vtable-pointer-address-discrimination \
         -fptrauth-vtable-pointer-address-discrimination -fptrauth-vtable-pointer-type-discrimination \
         -frtti \
-        -arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -stdlib=libc++ -fPIC
+        -arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -fPIC
         #-fno-exceptions
 
 objdump -d research_machine_code.o | tee output_research_machine_code.txt
@@ -14,7 +14,7 @@ clang++ -o prog research_machine_code.cpp -std=c++20 -O3 -DNDEBUG \
         -fptrauth-vtable-pointer-address-discrimination \
         -fptrauth-vtable-pointer-address-discrimination -fptrauth-vtable-pointer-type-discrimination \
         -frtti \
-        -arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -stdlib=libc++ -fPIC
+        -arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -fPIC
         #-fno-exceptions
 
 echo ====================== start of program ===========================
