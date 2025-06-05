b3807022dc97d88fb761b5e655aef6d282a59c9a
healytpk <thomaspkhealy@yahoo.com>
2025-06-06 00:51:56 +0100

    compiler option for authenticated GOT table

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,8 +2,9 @@
 clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -DNDEBUG -O3 -frtti \
         -fptrauth-vtable-pointer-address-discrimination \
         -fptrauth-vtable-pointer-type-discrimination \
-        -fptrauth-type-info-vtable-pointer-discrimination
-        #-fptrauth-calls
+        -fptrauth-type-info-vtable-pointer-discrimination \
+        -fptrauth-elf-got \
+        -fptrauth-calls
         #-arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -fPIC
         #-fno-exceptions
 
@@ -12,8 +13,9 @@ objdump -d research_machine_code.o | tee output_research_machine_code.txt
 clang++ -o prog research_machine_code.cpp -std=c++20 -DNDEBUG -O3 -frtti \
         -fptrauth-vtable-pointer-address-discrimination \
         -fptrauth-vtable-pointer-type-discrimination \
-        -fptrauth-type-info-vtable-pointer-discrimination
-        #-fptrauth-calls
+        -fptrauth-type-info-vtable-pointer-discrimination \
+        -fptrauth-elf-got \
+        -fptrauth-calls
         #-arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -fPIC
         #-fno-exceptions
 
