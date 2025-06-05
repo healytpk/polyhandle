db3cfdf4d9f77eca514154b5d4aba2b79cf82bca
healytpk <thomaspkhealy@yahoo.com>
2025-06-05 23:24:56 +0100

    Compile with pointer authentication

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -O3 -DNDEBUG
+clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -O3 -DNDEBUG \
+        -fptrauth-calls \
+        -fptrauth-vtable-pointer-address-discrimination \
+        -fptrauth-vtable-pointer-address-discrimination -fptrauth-vtable-pointer-type-discrimination \
+        #-fno-exceptions
+
 objdump -d research_machine_code.o | tee output_research_machine_code.txt
