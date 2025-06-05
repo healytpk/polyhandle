3de5739a2f52019b799c078661d5b2188b606be2
healytpk <thomaspkhealy@yahoo.com>
2025-06-06 00:09:21 +0100

    more compiler optionss

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,6 +3,8 @@ clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -O3 -
         -fptrauth-calls \
         -fptrauth-vtable-pointer-address-discrimination \
         -fptrauth-vtable-pointer-address-discrimination -fptrauth-vtable-pointer-type-discrimination \
+        -frtti \
+        -arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -stdlib=libc++ -fPIC
         #-fno-exceptions
 
 objdump -d research_machine_code.o | tee output_research_machine_code.txt
@@ -11,6 +13,8 @@ clang++ -o prog research_machine_code.cpp -std=c++20 -O3 -DNDEBUG \
         -fptrauth-calls \
         -fptrauth-vtable-pointer-address-discrimination \
         -fptrauth-vtable-pointer-address-discrimination -fptrauth-vtable-pointer-type-discrimination \
+        -frtti \
+        -arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -stdlib=libc++ -fPIC
         #-fno-exceptions
 
 echo ====================== start of program ===========================
