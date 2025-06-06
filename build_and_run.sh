17d67ff1dc179b19add92c0ca898e61ebef4a5d7
healytpk <thomaspkhealy@yahoo.com>
2025-06-06 10:22:19 +0100

    arch now set to arm64e

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
-clang++ -o research_machine_code.o -c research_machine_code.cpp -std=c++20 -DNDEBUG -O3 -fno-rtti -fno-exceptions \
-        -fptrauth-calls
-        #-fptrauth-vtable-pointer-address-discrimination \
-        #-fptrauth-vtable-pointer-type-discrimination \
-        #-fptrauth-type-info-vtable-pointer-discrimination \
-        #-arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -fPIC
-        #-fno-exceptions
+clang++ -o research_machine_code.o -c research_machine_code.cpp \
+        -std=c++20 -arch arm64e -DNDEBUG -O3 -fno-rtti -fno-exceptions \
+        -fptrauth-calls \
+        -fptrauth-vtable-pointer-address-discrimination \
+        -fptrauth-vtable-pointer-type-discrimination \
+        -fptrauth-type-info-vtable-pointer-discrimination
 
 objdump -d research_machine_code.o | tee output_research_machine_code.txt
 
-clang++ -o prog research_machine_code.cpp -std=c++20 -DNDEBUG -O3 -fno-rtti -fno-exceptions \
-        -fptrauth-calls
-        #-fptrauth-vtable-pointer-address-discrimination \
-        #-fptrauth-vtable-pointer-type-discrimination \
-        #-fptrauth-type-info-vtable-pointer-discrimination \
-        #-arch arm64 -target arm64-apple-macos -mcpu=apple-m1 -fPIC
-        #
+clang++ -o prog research_machine_code.cpp \
+        -std=c++20 -arch arm64e-arch -DNDEBUG -O3 -fno-rtti -fno-exceptions \
+        -fptrauth-calls \
+        -fptrauth-vtable-pointer-address-discrimination \
+        -fptrauth-vtable-pointer-type-discrimination \
+        -fptrauth-type-info-vtable-pointer-discrimination
 
 echo ====================== start of program ===========================
 ./prog
