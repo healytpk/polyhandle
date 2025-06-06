02e60b62dd7f66cacf9d31c7e2d4991523417e23
healytpk <thomaspkhealy@yahoo.com>
2025-06-06 10:45:08 +0100

    Another compiler tweak

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 clang++ -o research_machine_code.o -c research_machine_code.cpp \
-        -std=c++20 -arch arm64e -DNDEBUG -O3 -fno-rtti -fno-exceptions \
-        -fptrauth-calls \
-        -fptrauth-vtable-pointer-address-discrimination \
-        -fptrauth-vtable-pointer-type-discrimination \
-        -fptrauth-type-info-vtable-pointer-discrimination
+        -std=c++20 -arch arm64e -DNDEBUG -O3
+        #-fno-rtti -fno-exceptions \
+        #-fptrauth-calls \
+        #-fptrauth-vtable-pointer-address-discrimination \
+        #-fptrauth-vtable-pointer-type-discrimination \
+        #-fptrauth-type-info-vtable-pointer-discrimination
 
 objdump -d research_machine_code.o | tee output_research_machine_code.txt
 
 clang++ -o prog research_machine_code.cpp \
-        -std=c++20 -arch arm64e -DNDEBUG -O3 -fno-rtti -fno-exceptions \
-        -fptrauth-calls \
-        -fptrauth-vtable-pointer-address-discrimination \
-        -fptrauth-vtable-pointer-type-discrimination \
-        -fptrauth-type-info-vtable-pointer-discrimination
+        -std=c++20 -arch arm64e -DNDEBUG -O3
+        #-fno-rtti -fno-exceptions \
+        #-fptrauth-calls \
+        #-fptrauth-vtable-pointer-address-discrimination \
+        #-fptrauth-vtable-pointer-type-discrimination \
+        #-fptrauth-type-info-vtable-pointer-discrimination
 
 echo ====================== start of program ===========================
 ./prog
